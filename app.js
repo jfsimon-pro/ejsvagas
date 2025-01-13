f31353914c29796d11f40560b98c578b73012fb6
Simon <77901095+jfsimon-pro@users.noreply.github.com>
2025-01-13 01:52:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -61,5 +61,9 @@ app.use((err, req, res, next) => {
 
 // Iniciar o servidor
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  if (err) {
+    console.error("Erro ao iniciar o servidor:", err);
+  } else {
+    console.log("Servidor rodando em http://localhost:3000");
+  }
 });
